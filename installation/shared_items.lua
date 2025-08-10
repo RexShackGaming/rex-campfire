777fd8426d9a58c1e7d56bdc079eba905269f40b
RexShack <rexshackuk@gmail.com>
2025-08-10 09:08:37 +0100

    Initial commit

---
+++ installation/shared_items.lua
@@ -0,0 +1,8 @@
+    -----------------------------------------------
+    -- YOUR CUSTOM ITEMS
+    -----------------------------------------------
+    campfire    = { name = 'campfire',    label = 'Camp Fire',   weight = 500, type = 'item', image = 'campfire.png',    unique = false, useable = true,  shouldClose = true, description = 'add your description'},
+    raw_meat    = { name = 'raw_meat',    label = 'Raw Meat',    weight = 50,  type = 'item', image = 'raw_meat.png',    unique = false, useable = false, shouldClose = true, description = 'add your description'},
+    raw_fish    = { name = 'raw_fish',    label = 'Raw Fish',    weight = 50,  type = 'item', image = 'raw_fish.png',    unique = false, useable = false, shouldClose = true, description = 'add your description'},
+    cooked_meat = { name = 'cooked_meat', label = 'Cooked Meat', weight = 50,  type = 'item', image = 'cooked_meat.png', unique = false, useable = true,  shouldClose = true, description = 'add your description'},
+    cooked_fish = { name = 'cooked_fish', label = 'Cooked Fish', weight = 50,  type = 'item', image = 'cooked_fish.png', unique = false, useable = true,  shouldClose = true, description = 'add your description'},
